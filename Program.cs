023c8eafde46b0f7291521d88a47512534a59064
JoãoMNS <joao.matheus.n.silva@gmail.com>
2026-02-03 12:16:41 -0300

    Finalizado desafio POO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone:");
+Iphone iphone = new Iphone("(71) 99452-7862", "iphone 13", "IMEI1234567890", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+Console.WriteLine();
+Console.WriteLine("Smartphone Nokia:");
+Nokia nokia = new Nokia("(71) 99876-5432", "Nokia G20", "IMEI0987654321", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
